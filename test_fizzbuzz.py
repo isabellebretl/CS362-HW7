2450cd0242a60d10b448bb5f6678ed113c93dc09
Isabelle Bretl <bretli@oregonstate.edu>
2021-05-31 09:25:45 -0700

    first test fails because fizzbuzz isn't written

---
+++ test_fizzbuzz.py
@@ -0,0 +1,10 @@
+import unittest
+import fizzbuzz
+
+class TestCase(unittest.TestCase):
+    def test_fb1(self):
+        for i in [3, 6, 9, 12]:
+            self.assertEqual(fizzbuzz(i), "Fizz")
+
+if __name__ == '__main__':
+    unittest.main()
