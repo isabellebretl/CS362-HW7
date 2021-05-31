c2145f213aed72e44e96d6054caebfc8f80ad781
Isabelle Bretl <bretli@oregonstate.edu>
2021-05-31 09:58:33 -0700

    wrote third test to check that years that are divisible by 400 return true

--- test_leapyear.py
+++ test_leapyear.py
@@ -8,6 +8,9 @@ class TestCase(unittest.TestCase):
     def test_ly2(self):
         for i in [2100, 1700, 900]:
             self.assertEqual(leapyear.leapyear(i), False)
+    def test_ly3(self):
+        for i in [2000, 1600, 2400]:
+            self.assertEqual(leapyear.leapyear(i), True)
 
 if __name__ == '__main__':
     unittest.main()
