e3743cdd6a33daf483c653385b49cbe70b77dbbd
Isabelle Bretl <bretli@oregonstate.edu>
2021-05-31 09:53:44 -0700

    fixed minor error for test_ly1 on line 7

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,7 +4,7 @@ import leapyear
 class TestCase(unittest.TestCase):
     def test_ly1(self):
         for i in [2004, 1996, 1816]:
-            self.assertEqual(leapyear.leapyear(), True)
+            self.assertEqual(leapyear.leapyear(i), True)
 
 if __name__ == '__main__':
     unittest.main()
