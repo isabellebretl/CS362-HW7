4509693a4a8bca7292ba078281a1384ffbbd7b77
Isabelle Bretl <bretli@oregonstate.edu>
2021-05-31 09:32:41 -0700

    wrote second test, to test multiples of 5 printing out Buzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,6 +5,9 @@ class TestCase(unittest.TestCase):
     def test_fb1(self):
         for i in [3, 6, 9, 12]:
             self.assertEqual(fizzbuzz.fizzbuzz(i), "Fizz")
+    def test_fb2(self):
+        for i in [5, 10, 15, 20]:
+            self.assertEqual(fizzbuzz.fizzbuzz(i), "Buzz")
 
 if __name__ == '__main__':
     unittest.main()
