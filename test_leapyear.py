281224897ce28a6fe5f2c365a92b8bf4d1d2dcf0
Isabelle Bretl <bretli@oregonstate.edu>
2021-05-31 09:52:26 -0700

    first test written to check years only divisible by 4

---
+++ test_leapyear.py
@@ -0,0 +1,10 @@
+import unittest
+import leapyear
+
+class TestCase(unittest.TestCase):
+    def test_ly1(self):
+        for i in [2004, 1996, 1816]:
+            self.assertEqual(leapyear.leapyear(), True)
+
+if __name__ == '__main__':
+    unittest.main()
