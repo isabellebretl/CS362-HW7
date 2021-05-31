52b54ecaaf5dc2836923ed7eda45be4c25939879
Isabelle Bretl <bretli@oregonstate.edu>
2021-05-31 09:55:52 -0700

    wrote second test to check for years that are evenly divisible by 100 to return false

--- test_leapyear.py
+++ test_leapyear.py
@@ -5,6 +5,9 @@ class TestCase(unittest.TestCase):
     def test_ly1(self):
         for i in [2004, 1996, 1816]:
             self.assertEqual(leapyear.leapyear(i), True)
+    def test_ly2(self):
+        for i in [2100, 1700, 900]:
+            self.assertEqual(leapyear.leapyear(i), False)
 
 if __name__ == '__main__':
     unittest.main()
