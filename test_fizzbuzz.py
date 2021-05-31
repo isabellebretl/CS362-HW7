4122ddc5529d6781bc42addf8fc7dbf4416b8bfc
Isabelle Bretl <bretli@oregonstate.edu>
2021-05-31 09:30:19 -0700

    fixed minor error in test on line 7

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,7 +4,7 @@ import fizzbuzz
 class TestCase(unittest.TestCase):
     def test_fb1(self):
         for i in [3, 6, 9, 12]:
-            self.assertEqual(fizzbuzz(i), "Fizz")
+            self.assertEqual(fizzbuzz.fizzbuzz(i), "Fizz")
 
 if __name__ == '__main__':
     unittest.main()
